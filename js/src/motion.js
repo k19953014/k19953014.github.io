08f773b8f8ca5f7fa6f90f3421e09339629c596c
k19953014 <k19831107@gmail.com>
2018-07-11 20:50:28 +0800

    Site updated: 2018-07-11 20:50:27

--- js/src/motion.js
+++ js/src/motion.js
@@ -51,28 +51,49 @@ $(document).ready(function () {
     this.el.velocity('stop').velocity(this.status[status]);
   };
 
-  var sidebarToggleLine1st = new SidebarToggleLine({
-    el: '.sidebar-toggle-line-first',
-    status: {
-      arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
-      close: {width: '100%', rotateZ: '-45deg', top: '5px'}
-    }
-  });
-  var sidebarToggleLine2nd = new SidebarToggleLine({
-    el: '.sidebar-toggle-line-middle',
-    status: {
-      arrow: {width: '90%'},
-      close: {opacity: 0}
-    }
-  });
-  var sidebarToggleLine3rd = new SidebarToggleLine({
-    el: '.sidebar-toggle-line-last',
-    status: {
-      arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
-      close: {width: '100%', rotateZ: '45deg', top: '-5px'}
-    }
-  });
-
+  // var sidebarToggleLine1st = new SidebarToggleLine({
+    // el: '.sidebar-toggle-line-first',
+    // status: {
+      // arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
+      // close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+    // }
+  // });
+  // var sidebarToggleLine2nd = new SidebarToggleLine({
+    // el: '.sidebar-toggle-line-middle',
+    // status: {
+      // arrow: {width: '90%'},
+      // close: {opacity: 0}
+    // }
+  // });
+  // var sidebarToggleLine3rd = new SidebarToggleLine({
+    // el: '.sidebar-toggle-line-last',
+    // status: {
+      // arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
+      // close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+    // }
+  // });
+var sidebarToggleLine1st = new SidebarToggleLine({
+  el: '.sidebar-toggle-line-first',
+ 	status: {
+   	arrow: {width: '50%', rotateZ: '45deg', top: '2px', left: '5px'},
+   	close: {width: '100%', rotateZ: '45deg', top: '5px', left: 0}
+ 	}
+});
+var sidebarToggleLine2nd = new SidebarToggleLine({
+ 	  el: '.sidebar-toggle-line-middle',
+ 	status: {
+   	arrow: {width: '90%'},
+   	close: {opacity: 0}
+ 	}
+});
+var sidebarToggleLine3rd = new SidebarToggleLine({
+ 	  el: '.sidebar-toggle-line-last',
+ 	status: {
+   	arrow: {width: '50%', rotateZ: '-45deg', top: '-2px', left: '5px'},
+   	close: {width: '100%', rotateZ: '-45deg', top: '-5px', left: 0}
+ 	}
+});
+  
   sidebarToggleLines.push(sidebarToggleLine1st);
   sidebarToggleLines.push(sidebarToggleLine2nd);
   sidebarToggleLines.push(sidebarToggleLine3rd);
@@ -98,7 +119,8 @@ $(document).ready(function () {
       $(document)
         .on('sidebar.isShowing', function () {
           NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
-            {paddingRight: SIDEBAR_WIDTH},
+            // {paddingRight: SIDEBAR_WIDTH},
+			{paddingLeft: SIDEBAR_WIDTH},
             SIDEBAR_DISPLAY_DURATION
           );
         })
@@ -164,7 +186,8 @@ $(document).ready(function () {
       this.sidebarEl.trigger('sidebar.isShowing');
     },
     hideSidebar: function () {
-      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+      // NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+	  NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingLeft: 0});
       this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
       this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
 
