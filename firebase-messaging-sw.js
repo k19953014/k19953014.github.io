0991bbb49b1504c6edd5e2bd323460e0d75c86d8
pita_lin <pita_lin@gss.com.tw>
2025-02-05 15:13:47 +0800

    3123123

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -81,10 +81,11 @@ self.addEventListener("notificationclick", (event) => {
     const url = event.notification.data.url;
     const action = event.notification.data.action;
     const msg = event.notification.data.oriMessage;
+    const guid = event.notification.data.guid;
     const messageText = encodeURIComponent(msg);
 
     if (url) {
-      clients.openWindow(url+'?msg='+messageText);
+      clients.openWindow(`${url}?msg=${messageText}&guid=${guid}`);
     }
 
 
